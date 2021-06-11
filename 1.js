742e27eb79ce771da0148fb03d57d04de2df3041
Your Name <bosin233@gmail.com>
2021-06-11 14:45:34 +0700

    jquery

--- 1.js
+++ 1.js
@@ -1,29 +1,34 @@
-$(document).ready(function() {
-	
-	
-	$('.next').click(function(){
-		const imgselect = $('img.anh_1:visible');
-		let img = parseInt(imgselect.attr('number'));
-		let eqnumber = img-1 ;
-		$('img.anh_1').eq(eqnumber).fadeIn();
-		imgselect.fadeOut();
-		})
+	// $('.next').click(function(){
+	// 	const imgselect = $('img.anh_1:visible');
+	// 	let img = parseInt(imgselect.attr('number'));
+	// 	let eqnumber = img-1 ;
+	// 	$('img.anh_1').eq(eqnumber).fadeIn();
+	// 	imgselect.fadeOut();
+	// 	})
 
-	$('.prev').click(function(){
-		const imgselect = $('img.anh_1:visible');
-		let img = parseInt(imgselect.attr('number'));
-		let eqnumber = img - 1;
-		$('img.anh_1').eq(eqnumber).fadeIn();
-		imgselect.fadeOut();
-		})
+	// $('.prev').click(function(){
+	// 	const imgselect = $('img.anh_1:visible');
+	// 	let img = parseInt(imgselect.attr('number'));
+	// 	let eqnumber = img - 1;
+	// 	$('img.anh_1').eq(eqnumber).fadeIn();
+	// 	imgselect.fadeOut();
+	// 	})
+
+	$(document).ready(function() {
+	$('.btn').click(function(event) {
+		$('.hop-thoai').addClass('hienra');
+		$('.nen_1').addClass('hienra');
+	});
+	$('.btn-close').click(function(event) {
+		$('.hop-thoai').removeClass('hien-ra');
+		$('.nen-mo').removeClass('hien-ra');
+	});
+	$('.nen-mo').click(function(event) {
+		$('.hop-thoai').removeClass('hien-ra');
+		$('.nen-mo').removeClass('hien-ra');
+	});
+});
 
 
-	
-	
-	
-	
-
-
-})
 
 
